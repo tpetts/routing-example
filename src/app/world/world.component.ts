10a602d511b3e2ec5ec90190e91ce8d50d44e452
tpetts <tpetts@madisoncollege.edu>
2021-12-07 11:22:23 -0600

    Injecting of the dependencies!
    Now the world component has access to the greeting service!

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GreetingService } from '../greeting.service';
 
 @Component({
   selector: 'app-world',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorldComponent implements OnInit {
 
-  constructor() { }
+  constructor(public greetingSvc: GreetingService) { }
 
   ngOnInit(): void {
   }

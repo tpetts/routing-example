82c0efb3e60f5b329dd3c6d5212aa0ee434d7f94
tpetts <tpetts@madisoncollege.edu>
2021-12-07 11:00:39 -0600

    Adding some more routes
    defining the paths to add to the end of the dns and connecting the component that will render on the screen.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KittyComponent } from './kitty/kitty.component';
 import { WorldComponent } from './world/world.component';
 
 const routes: Routes = [
   {
     path: ''
     , component: WorldComponent
+  }
+  , {
+    path: 'world'
+    , component: WorldComponent
+  }
+  , {
+    path: 'kitty'
+    , component: KittyComponent
   }
 ];
 

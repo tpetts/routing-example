8f0a927ccbc0ae181941139378b7ffec77884577
tpetts <tpetts@madisoncollege.edu>
2021-12-07 10:50:48 -0600

    Ran 3 Generate commands
    Generated 2 components and a service.
    ng generate component world --skip-tests
    
    ng generate component kitty --skip-tests
    
    ng generate service greeting --skip-tests

---
+++ src/app/kitty/kitty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitty',
+  templateUrl: './kitty.component.html',
+  styleUrls: ['./kitty.component.css']
+})
+export class KittyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

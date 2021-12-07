b891f29e9bcbf5e00e7d87b2cbf9c151116de9f3
tpetts <tpetts@madisoncollege.edu>
2021-12-07 11:23:43 -0600

    Injecting Dependencies, part II
    Now the kitty component has access to the greeting service

--- src/app/kitty/kitty.component.ts
+++ src/app/kitty/kitty.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GreetingService } from '../greeting.service';
 
 @Component({
   selector: 'app-kitty',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KittyComponent implements OnInit {
 
-  constructor() { }
+  constructor(public kittyGreeting: GreetingService) { }
 
   ngOnInit(): void {
   }
